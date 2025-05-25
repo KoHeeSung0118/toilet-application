498cb29de6732ca34e9c82582acbdaa23042451c
KoHeeSung0118 <heesung3597@naver.com>
2025-05-25 15:31:39 +0900

    first commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ export default async function signup() {
       <form action="/api/post/login" method="POST">
         <input name="email" placeholder="이메일" />
         <input name="password" placeholder="비밀번호" />
-        <button type="submit">로그인</button>
+        <button type="submit">로그인 버튼</button>
       </form>
       <button style={{
         marginTop: '20px'
