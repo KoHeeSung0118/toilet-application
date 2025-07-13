ffd7e572242f70b584fe48635b77abf9fa7ab05f
KoHeeSung0118 <heesung3597@naver.com>
2025-07-13 14:32:41 +0900

    Update page.tsx
    app/page.tsx수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+// ✅ app/page.tsx  또는 app/(root)/page.tsx
+"use client";                                  // ← 한 줄 추가
+
 import MapView from "@/components/map/MapView";
 
 export default function Mainpage() {
   return (
     <div className="bottombar">
-      <MapView></MapView>
+      <MapView />
     </div>
-  )
+  );
 }
